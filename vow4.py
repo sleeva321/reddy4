c959ba1871071bab464f0eb11d8e06d5876c8c43
D.siva reddy <40803761+sleeva321@users.noreply.github.com>
2018-07-06 22:45:56 +0530

    Create vow4.py

---
+++ vow4.py
@@ -0,0 +1,7 @@
+ch=str(raw_input())
+if ch in ['a','e','i','o','u']:
+    print"Vowel"
+elif ch  in ['b','c','d','f','g','h','j','k','l','m','n','p','q','s','t','v','w','x','y','z']:
+    print"consonant"
+else:
+    print"invalid"
